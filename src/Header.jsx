1ffb7aadc444fa3591d820a62457402677e91b72
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-07-27 21:30:43 +0200

    oups

--- src/Header.jsx
+++ src/Header.jsx
@@ -8,7 +8,7 @@ export const Header = () => {
     <header className="flex w-full items-center py-4">
       <h1 className="inline-flex items-center ">
         <ReactSvg size="32" />
-        <span className="pl-3 font-bold">React Journey</span>
+        <span className="pl-3 font-bold">React Journey !</span>
       </h1>
       <div className="flex items-center ml-auto gap-2">
         <Button variant="ghost">
